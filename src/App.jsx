90a8d34dc9363454c9e3b941ece8ad26afe08c1e
SandhanuDulmeth <sandhanu12@gmail.com>
2025-02-05 20:32:31 +0530

    style: add navigation links to App component for Home and Products

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react'
 import './App.css'
 import Product from './Products'
 import Home from './Home'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 
 
 function App() {
@@ -13,15 +13,21 @@ function App() {
   return (
     <>
       <h1>Hello</h1>
-     <BrowserRouter>
-     <Routes>
-        <Route path="/home" element={<Home/>} />
-        <Route path="/products" element={<Product/>} />
-     </Routes>
-     </BrowserRouter>
-    
 
-   
+
+      <BrowserRouter>
+
+        <Link to="/home">Home</Link><br />
+        <Link to="/products">Products</Link>
+
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/products" element={<Product />} />
+        </Routes>
+      </BrowserRouter>
+
+
+
     </>
   )
 }
