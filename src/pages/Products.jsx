9dfab02b93d69f21f5f2c204c9171db5a657396f
SandhanuDulmeth <sandhanu12@gmail.com>
2025-02-06 18:11:47 +0530

    fix: update price of Iphone 15 in Products component for accuracy

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -4,7 +4,7 @@ export default function Product() {
         <>
             <div className="product">
 
-                <ProductItem name="Iphone 15" price={7800} />
+                <ProductItem name="Iphone 15" price={7700} />
                 <ProductItem name="Iphone 14" price={6900} />
                 <ProductItem name="Iphone 13" price={5880} />
             </div>
