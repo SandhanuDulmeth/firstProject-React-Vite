50b8622282ef7eeb9e77da2dd2202944a1886787
SandhanuDulmeth <sandhanu12@gmail.com>
2025-02-06 12:08:53 +0530

    style: add button to Home component for interaction and logging

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,24 @@
 export default function Home() {
+
+
+function printSomthing() { 
+       console.log("I am clicked");
+  }
+
+
+
+
+
     return (
         <>
-            <div className="home">Welcome to the home page!</div>
+            <div className="home">
+                Welcome to the home page!
+                <br />
+                <button onClick={printSomthing}>click Me </button>
+                
+                
+                
+                </div>
         </>
     );
 }
