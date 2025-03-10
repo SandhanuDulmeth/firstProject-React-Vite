90643c883c5206d95f7fbc0cb9750b1fbd3e385d
SandhanuDulmeth <sandhanu12@gmail.com>
2025-03-10 10:28:55 +0530

    feat: enhance Products page with dynamic product listing and image display

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -2,6 +2,7 @@ export default function ProductItem(props) {
     return (
         <>
             <div className="product-item">
+                <img className="product-img" src={props.imageUrl} alt="" />
                 <h1>{props.name}</h1>
                 <h2>{props.price}</h2>
             </div>
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,12 +1,45 @@
+
 import ProductItem from '../components/ProductItem'
+import { useState } from 'react'
 export default function Product() {
+
+
+    let [data, setData] = useState([
+        { name: "Iphone 150", price: 7700 , imageUrl:"https://th.bing.com/th/id/OIP.HfDaw-1w0DpMnKa0e5FcBQHaHa?rs=1&pid=ImgDetMain" },
+        { name: "Iphone 140", price: 6900 , imageUrl:"https://th.bing.com/th/id/OIP.5TCgpn96fggMvcKV88JfkgHaHa?w=900&h=900&rs=1&pid=ImgDetMain" },
+        { name: "Iphone 130", price: 5880 , imageUrl:"https://th.bing.com/th/id/OIP.1RB7gxv-P_A5kr7phSKc6gHaHa?rs=1&pid=ImgDetMain" },
+    ]);
+
+    function getIphoneList() {
+        return data.map((item) => {
+            return (
+                <h1>{item.name + " , " + item.price}</h1>
+
+            )
+        })
+    }
+
     return (
         <>
             <div className="product">
 
-                <ProductItem name="Iphone 15" price={7700} />
+                {/* {getIphoneList()} */}
+
+
+                {/* <ProductItem name="Iphone 15" price={7700} />
                 <ProductItem name="Iphone 14" price={6900} />
-                <ProductItem name="Iphone 13" price={5880} />
+                <ProductItem name="Iphone 13" price={5880} /> */}
+
+                {
+                    data.map((item) => {
+                        return (
+                            <ProductItem name={item.name} price={item.price} imageUrl={item.imageUrl} />
+                        )
+
+                    })
+                }
+
+
             </div>
 
         </>
